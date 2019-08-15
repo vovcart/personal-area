77c1733d2d9d55f4adeb41a7eabd7b494c658b5f
V-Negura <negura.nr.five@gmail.com>
2019-08-15 14:46:22 +0300

    master: Did third task

--- build.sbt
+++ build.sbt
@@ -1,6 +1,11 @@
 scalaVersion := "2.12.8"
 
+//addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "x.y.z")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.0.3")
+
 libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.1.9"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.23"
 libraryDependencies += "com.typesafe" % "config" % "1.3.4"
 libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.9"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
--- src/main/scala/com/none/web_app/Api.scala
+++ src/main/scala/com/none/web_app/Api.scala
@@ -1,35 +1,49 @@
 package com.none.web_app
 
 import akka.http.scaladsl.server.Directives._
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Terminated}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
+import akka.http.scaladsl.Http.ServerBinding
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 object Api extends App {
 
+  val log = Logger(Api.getClass)
   //Initiate config first
   //And context
   lazy val config = ConfigFactory.load()
 
   lazy val host = config.getString("http.host")
+  log.info("Setted hostname: {}", host)
 
   lazy val port = config.getInt("http.port")
+  log.info("Setted port: {}", port)
 
   //Then environment context
   implicit val system = ActorSystem("my-system")
 
   implicit val materializer = ActorMaterializer()
 
-  //Routes
-  lazy val routes =
-    RouteOne().route ~
-    pathPrefix("routetwo") { RouteTwo().route }
+  implicit val executionContext = system.dispatcher
 
+  //Routes
+  lazy val route = RouteOne().route ~ RouteTwo().route
 
   //Start Http server
-  val route = Http().bindAndHandle(routes, host, port)
+  val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(route, host, port)
+  log.info("binded with {}: {}! ", host, port)
+  log.info("Server is up")
 
-  //TODO check if server is up
-  //In case of fail do shutdown stuff (read AKKA-HTTP suggestions for that) ... shutdown main thread
+  sys.addShutdownHook {
+    val terminate: Future[Terminated] = system.terminate()
+    Await.result(terminate, Duration("10 seconds"))
+    log.info("Server is not up")
+  }
+
+  log.info("Main thread id: "+Thread.currentThread().getId.toString)
 }
